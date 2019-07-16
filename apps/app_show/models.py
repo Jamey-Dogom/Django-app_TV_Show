6b4c553627b0992c3de5ba3d4b8f61fec08414e6
Jamey-Dogom <33010705+AppJedi@users.noreply.github.com>
2019-07-15 17:30:26 -0700

    validation of forms

--- apps/app_show/models.py
+++ apps/app_show/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # add keys and values to errors dictionary for each invalid field
+        if len(postData['title']) < 5:
+            errors['title'] = "Title should be at least 2 characters"
+        if len(postData['network']) < 3:
+            errors['network'] = "Network should be at least 3 characters"
+        if len(postData['description']) > 0 and len(postData['description']) < 10:
+            errors['description'] = "Description is optional, but if present must be at least 10 characters"
+        return errors
+
+
+
 class Show(models.Model):
     title = models.CharField(max_length=45)
     network = models.CharField(max_length=45)
@@ -8,7 +23,7 @@ class Show(models.Model):
     release_date = models.DateField('%m/%d/%Y')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
 
     def __repr__(self):
         return f"<Show object:  {self.id} {self.title} {self.network} {self.description}>"
-
--- apps/app_show/views.py
+++ apps/app_show/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from apps.app_show.models import *
 import datetime
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -16,16 +17,27 @@ def add_show(request):
     return render(request, "app_show/addshow.html")
 
 def create_show(request):
-    release = str(request.POST['release_date'])
-    release = datetime.datetime.strptime(release, "%m/%d/%Y").strftime("%Y-%m-%d")
+    # pass the post data to the method we wrote and save the response in a variable called errors
+    errors = Show.objects.basic_validator(request.POST)
+        # check if the errors dictionary has anything in it
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+        fix = '/shows/new'
+        return redirect(fix)
+    else:
+        release = str(request.POST['release_date'])
+        release = datetime.datetime.strptime(release, "%m/%d/%Y").strftime("%Y-%m-%d")
 
-    new_show = Show.objects.create(title = request.POST['title'], network = request.POST['network'], description = request.POST['description'], release_date = release)
+        new_show = Show.objects.create(title = request.POST['title'], network = request.POST['network'], description = request.POST['description'], release_date = release)
 
-    show_id = new_show.id
-    urlll = '/shows/{}'.format(show_id)
-    print(urlll)
+        show_id = new_show.id
+        urlll = '/shows/{}'.format(show_id)
+        print(urlll)
 
-    return redirect(urlll)
+        return redirect(urlll)
 
 def display_show(request, show_id):
     show = Show.objects.get(id = show_id)
@@ -42,13 +54,24 @@ def update_show(request, show_id):
     return render(request, "app_show/updateshow.html", context)
 
 def update(request, show_id):
-    release = str(request.POST['release_date'])
-    release = datetime.datetime.strptime(release, "%m/%d/%Y").strftime("%Y-%m-%d")
+    
+    errors = Show.objects.basic_validator(request.POST)
+    # check if the errors dictionary has anything in it
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+        urlll = '/shows/{}/edit'.format(show_id)
+        return redirect(urlll)
+    else:
+        release = str(request.POST['release_date'])
+        release = datetime.datetime.strptime(release, "%m/%d/%Y").strftime("%Y-%m-%d")
 
-    Show.objects.filter(id=show_id).update(title = request.POST['title'], network = request.POST['network'], description = request.POST['description'], release_date = release)
+        Show.objects.filter(id=show_id).update(title = request.POST['title'], network = request.POST['network'], description = request.POST['description'], release_date = release)
 
-    urlll = '/shows/{}'.format(show_id)
-    return redirect(urlll)
+        urlll = '/shows/{}'.format(show_id)
+        return redirect(urlll)
 
 def delete(request, show_id):
     print("Deleting")
